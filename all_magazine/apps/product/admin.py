f12bfdedd55c0965d14996bb20eb0255eb0413a1
LangaMAd <artykovh@gmail.com>
2024-09-12 20:46:22 +0600

    add api product

--- all_magazine/apps/product/admin.py
+++ all_magazine/apps/product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(TestProduct)
+class TestProductAdmin(admin.ModelAdmin):
+    list_display = ['name','articul','price',
+                    'description','image','is_active']
+
+
--- all_magazine/core/settings.py
+++ all_magazine/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
+    'rest_framework',
     'apps.accounts',
     'apps.product',
     'apps.cart',
